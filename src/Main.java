af6ba32c742708c79edafb3680055fcfb9a58ad3
welandfr <welandfr@arcada.fi>
2026-01-14 15:32:03 +0200

    lektion 1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello");
+
+        Car car = new Car("VW");
+        Car car2 = new Car("Tesla");
+
+        System.out.println(car.getName());
+        car.setName("Volvo");
+        System.out.println(car.getName());
+        System.out.println(car2.getName());
+
     }
 
 }
