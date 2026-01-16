a63c72dfb1504fc7abcfe0be9c863255459bfbd3
welandfr <welandfr@arcada.fi>
2026-01-16 11:59:06 +0200

    lektion 2

--- src/Car.java
+++ src/Car.java
@@ -1,8 +1,30 @@
 public class Car extends Vehicle {
 
+    private int fuel = 0;
+
+    // Overload (överlagring)
+    // behävs för att tillåta olika antal parametrar
+    public Car(String name, double price) {
+        super(name, "motorized");
+        super.price = price;
+        super.discount = 0.9499999;
+    }
     public Car(String name) {
-        super.name = name;
-        super.vehicleType = "motorized";
+        super(name, "motorized");
+
+    }
+
+    @Override
+    public String soundWarning() {
+        return "tut-tut";
+    }
+
+    // overload
+    public void refuel(int amount) {
+        fuel += amount;
+    }
+    public void refuel() {
+        fuel = 100;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,47 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
 
-        Car car = new Car("VW");
-        Bike bike = new Bike("Scott");
+        Shirt shirt = new Shirt(50);
+        Hat hat = new Hat(20);
+
+        System.out.printf("Skortans skick: %d\n", shirt.getCondition());
+        shirt.mend();
+        System.out.printf("Skortans skick: %d\n", shirt.getCondition());
+
+        hat.mend();
+
+        Vehicle car = new Car("VW", 12000);
+        Vehicle bike = new Bike("Scott");
+        Car car2 = new Car("Volvo");
+        Vehicle todaysVehicle;
+
+        //Vehicle boat = new Vehicle("Buster", "båt");
+
+        car.setPrice(10000);
+        System.out.printf("%s kostar %.2f €\n",
+                car.getName(),
+                car.getPrice()
+        );
+
+        System.out.println(car.getName() + " kostar " + car.getPrice() + " €");
+
+        todaysVehicle = car;
+        System.out.printf("I dag åker jag %s\n", todaysVehicle.getName());
+        todaysVehicle = bike;
+        System.out.printf("I morgon åker jag %s\n", todaysVehicle.getName());
+
+        // overload
+        car2.refuel();
+        car2.refuel(50);
+
 
         System.out.println(car.getName() + " is " + car.getVehicleType());
         System.out.println(bike.getName() + " is " + bike.getVehicleType());
 
 
+
+        System.out.println(car.getName() + " säger " + car.soundWarning());
+        System.out.println(bike.getName() + " säger " + bike.soundWarning());
 
 
     }
