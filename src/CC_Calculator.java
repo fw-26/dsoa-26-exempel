6fc91f1841b376aa58dc9e3df7fcaec938c6acea
Fredrik Welander <welandfr@arcada.fi>
2026-01-23 10:03:07 +0200

    mellancommit

---
+++ src/CC_Calculator.java
@@ -0,0 +1,11 @@
+public class CC_Calculator {
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int multiply(int a, int b, int c) {
+        return a * b * c;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Scanner textInput = new Scanner(System.in);
+        /*
+        System.out.print("Skriv någonting: ");
+        String str = textInput.nextLine();
+        System.out.printf("Du skrev: %s\n", str);
+        */
 
         BikeShop bikeShop = new BikeShop();
         bikeShop.addBike(new Bike("Scott", 500));
         bikeShop.getBike(0).addSpec("wheel-size", "29 inch");
         bikeShop.getBike(0).addSpec("suspension", "hardtail");
-
-        System.out.println(bikeShop.getBike(0).getSpecsString());
 
         bikeShop.addBike(new Bike("Jopo", 200));
         bikeShop.getBike(1).addSpec("wheel-size", "24 inch");
@@ -20,35 +27,35 @@ public class Main {
         System.out.println("Välkommen till Bike Shop! Våra cyklar:");
         for (int i = 0; i < bikeShop.getBikeCount(); i++) {
             Bike bike = bikeShop.getBike(i);
-            System.out.printf("%s %.2f saldo: %d\n%s",
+            System.out.printf("%d - %s %.2f € saldo: %d\n%s",
+                    i+1,
                     bike.getName(),
                     bike.getPrice(),
                     bike.getStock(),
                     bike.getSpecsString());
         }
 
-        System.exit(0);
+        System.out.printf("\nVilken cykel vill du köpa? (1-%d): ",
+                bikeShop.getBikeCount());
+        String bikeInput = textInput.nextLine();
 
-        Car car = new Car("VW", 12000);
-        Car car2 = new Car("Volvo", 22000);;
+        try {
+            // Integer.parseInt(str) ändrar String till int
+            int chosenBike = Integer.parseInt(bikeInput)-1;
+            Bike bikeOwned = bikeShop.getBike(chosenBike);
+            System.out.printf("Grattis, du äger nu cykel %s\n", bikeOwned.getName());
 
-        car.refuel(50);
-
-        System.out.printf("%s %s)\n",
-                car.getName(),
-                car.getFuelInfo());
-
-        car2.refuel(100);
-        System.out.printf("%s %s\n",
-                car2.getName(),
-                car2.getFuelInfo()
-        );
-
-        Vehicle bike = new Bike("Scott");
+        } catch (NumberFormatException e) {
+            System.out.println("Du måste ge ett heltal!");
+        }
 
 
-
-
+        // Code Challenge
+        /*
+        CC_Calculator calculator = new CC_Calculator();
+        System.out.println(calculator.multiply(5, 4));
+        System.out.println(calculator.multiply(7, 6, 3));
+         */
 
     }
 
