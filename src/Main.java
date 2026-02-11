df26c05ac0510d956693efb596740876094c73d2
welandfr <welandfr@arcada.fi>
2026-02-11 12:00:15 +0200

    add linked list

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,30 @@ public class Main {
 
         String[] testData = { "Frodo","Samwise","Gandalf","Aragorn","Legolas","Gimli","Boromir","Meriadoc Brandybuck","Peregrin Took","Arwen","Galadriel","Elrond","Saruman","Gollum","Éowyn","Éomer","Faramir","Théoden","Bilbo Baggins","Treebeard","Denethor","Shelob","Radagast","Haldir","Celeborn","Gríma Wormtongue" };
 
-        StringArray names = new StringArray(testData);
+        Person p1 = new Person(testData[3]);
+        Person p2 = new Person(testData[2], p1);
+        Person p3 = new Person(testData[1], p2);
+        Person p4 = new Person(testData[0], p3);
 
+        // Vi lägger till ett element med O(1)
+        Person pMid = new Person("mid-person", p2);
+        p3.next = pMid;
+
+        Person current = p4;
+        while (current != null) {
+            System.out.println(current.getName());
+            current = current.next;
+        }
+
+
+
+
+
+
+
+        System.exit(0);
+
+        StringArray names = new StringArray(testData);
         System.out.println(names.toString());
         names.append("Sauron");
         System.out.println(names.toString());
